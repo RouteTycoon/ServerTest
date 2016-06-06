b4bb3b08516d04c4a1e11db455093f8f1b246df0
kmc7468 <kmc7468@naver.com>
2016-06-06 15:47:10 +0900

    Add files via upload

---
+++ ServerTest/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using TMR;
+
+namespace ServerTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			enterHostPort:
+			ushort port;
+			Console.Write("Open Port> ");
+			try
+			{
+				port = Convert.ToUInt16(Console.ReadLine().Trim());
+			}
+			catch
+			{
+				Console.WriteLine("Please re-enter.");
+				goto enterHostPort;
+			}
+
+			Server s = new Server();
+			s.Data.ServerName = "TestServer";
+			s.Data.MaxUserCount = 5;
+			s.Data.ServerImage = Utility.TransparentImage;
+
+			s.ReceiveMessage += new MessageEventHandler((e) => Console.WriteLine(e.Message.Text));
+			s.Start(port);
+
+			while (true)
+			{
+				string msg;
+				Console.Write("Message (Exit: X)> ");
+				msg = Console.ReadLine().Trim();
+
+				if (string.IsNullOrEmpty(msg))
+					continue;
+
+				if (msg.Equals("X"))
+				{
+					s.Stop();
+					Console.ReadLine();
+					return;
+				}
+
+				s.SendToAll(new Message() { Text = "[Server] " + msg, Type = MessageType.Chat });
+			}
+		}
+	}
+}
